8a6d90b8898fe771b7dc13b28ecd3a8f207879f5
Mehriban Khalilova <tu833KMN0@code.edu.az>
2023-11-20 06:33:50 +0400

    task completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/scss/main.scss'
-import Navbar from './layouts/Navbar'
+import { Home } from './pages/Home';
 
 function App() {
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <>
-      <Navbar/>
-    </>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
